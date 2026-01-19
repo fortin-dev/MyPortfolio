9de6605e9e17fc0781b6bc59b015b6f5e32aa161
Abhinav Yadav <abhiydv178@gmail.com>
2026-01-19 20:37:25 +0530

    My portfolio

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return(<>
+  <p class="text-primary bg-secondary font-display 3xl:text-lg">
+  This text uses custom theme colors and font.
+</p>
+</>)
+}
+
+export default App
